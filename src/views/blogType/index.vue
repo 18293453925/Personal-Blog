524aad6b99929b23a30a07fd4fd219c8bb111c48
清晨小半仙 <1058572981@qq.com>
2022-03-25 09:57:53 +0800

    作业
    第一次

---
+++ src/views/blogType/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>文章分类</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ src/views/set/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>轮播样式</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
